6b2138d82dd480255afe6a308cd5e5ff218a0a5f
Taras21071988 <tsasojlov@icloud.com>
2024-02-06 18:41:46 +0200

    making some changes to the files of attached pages

--- exercises/app/app.js
+++ exercises/app/app.js
@@ -1,3 +1,17 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".s-button-next",
+    prevEl: ".s-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
 const tabs = document.querySelectorAll(".lecture__week-item");
 const content = document.querySelectorAll(".week__content");
 
