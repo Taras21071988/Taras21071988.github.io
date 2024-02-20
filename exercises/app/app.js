c76ef77f7399560c468188fc59f705341ade60dd
Taras21071988 <tsasojlov@icloud.com>
2024-02-20 15:52:01 +0200

    rewriting the feed to download data from the server

--- exercises/app/app.js
+++ exercises/app/app.js
@@ -1,4 +1,9 @@
 const section = document.querySelector("section");
+const masterKey =
+  "$2a$10$3AK4oCmUXowJtaZ1b60Wi.s0Pa4RJ/uZ2rHsCLbKohxTPqQmt7dLq";
+
+const binId = `65d477b61f5677401f31cdd7/latest`;
+
 // поки не зміг розв'язати проблему з тим, щоб не створювати фіксовану кількість елементів
 for (let i = 0; i < 250; i++) {
   const span = document.createElement("span");
@@ -28,3 +33,80 @@ tabs.forEach((tab) => {
       .classList.add("selected");
   });
 });
+
+async function fetchData() {
+  try {
+    const response = await fetch(`https://api.jsonbin.io/v3/b/${binId}`, {
+      method: "GET",
+      headers: {
+        "X-Master-Key": masterKey,
+      },
+    });
+    const data = await response.json();
+    console.log(data.record.record[0]);
+    return data.record.record[0];
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function renderButtons(data) {
+  const weeks = Object.keys(data);
+
+  weeks.forEach((week, weekIndex) => {
+    let contentItem = document.querySelector(
+      `.week__content[data-id="${weekIndex}"] .content__item`
+    );
+
+    if (!contentItem) {
+      contentItem = document.createElement("div");
+      contentItem.className = "content__item";
+    }
+
+    const ulElement = document.createElement("ul");
+    ulElement.className = "lecture__list";
+
+    data[week].forEach((item, itemIndex) => {
+      const liElement = document.createElement("li");
+      liElement.className = "lecture__list-item";
+
+      if (item.url) {
+        const aElement = document.createElement("a");
+        aElement.className = "list__item-link";
+        aElement.href = item.url;
+        aElement.textContent = item.name;
+        liElement.appendChild(aElement);
+      } else {
+        const pItem = document.createElement("p");
+        pItem.textContent = "Урок пока не был проведен";
+        liElement.appendChild(pItem);
+      }
+
+      ulElement.appendChild(liElement);
+    });
+
+    contentItem.appendChild(ulElement);
+
+    const currentWeekContent = document.querySelector(
+      `.week__content[data-id="${weekIndex}"]`
+    );
+    if (currentWeekContent) {
+      currentWeekContent.appendChild(contentItem);
+    }
+  });
+}
+
+async function delayExecution() {
+  await new Promise((resolve) => setTimeout(resolve, 100));
+}
+async function fetchDataAndRenderButtons() {
+  await delayExecution();
+  const jsonData = await fetchData();
+  if (jsonData !== undefined && jsonData !== null) {
+    renderButtons(jsonData);
+  } else {
+    console.error("Данные не были успешно загружены.");
+  }
+}
+
+fetchDataAndRenderButtons();
--- exercises/page/app.js
+++ exercises/page/app.js
@@ -1,79 +1,83 @@
 const exitBtn = document.querySelector(".exit__btn");
+const masterKey =
+  "$2a$10$3AK4oCmUXowJtaZ1b60Wi.s0Pa4RJ/uZ2rHsCLbKohxTPqQmt7dLq";
+
+const binId = `65d477b61f5677401f31cdd7/latest`;
 
 exitBtn.addEventListener("click", function () {
   localStorage.removeItem("admin");
   window.location.href = "../index.html";
-  console.log("hi");
 });
 
-let data = [
-  {
-    weekOne: [
-      { name: "lection 1", url: "../" },
-      { name: "lection 2", url: "../" },
-      { name: "lection 3", url: "../" },
-    ],
-    weekTwo: [
-      { name: "lection 4", url: "../" },
-      { name: "lection 5", url: "../" },
-      { name: "lection 6", url: "../" },
-    ],
-    weekThree: [
-      { name: "lection 7", url: "../" },
-      { name: "lection 8", url: "../" },
-      { name: "lection 9", url: "../" },
-    ],
-    weekFour: [
-      { name: "lection 10", url: "../" },
-      { name: "lection 11", url: "../" },
-      { name: "lection 12", url: "../" },
-    ],
-    weekFive: [
-      { name: "lection 13", url: "../" },
-      { name: "lection 14", url: "../" },
-      { name: "lection 15", url: "../" },
-    ],
-    weekSix: [
-      { name: "lection 16", url: "../" },
-      { name: "lection 17", url: "../" },
-      { name: "lection 18", url: "../" },
-    ],
-    weekSeven: [
-      { name: "lection 19", url: "../" },
-      { name: "lection 20", url: "../" },
-      { name: "lection 21", url: "../" },
-    ],
-    weekEight: [
-      { name: "lection 22", url: "../" },
-      { name: "lection 23", url: "../" },
-      { name: "lection 24", url: "../" },
-    ],
-    weekNine: [
-      { name: "lection 25", url: "../" },
-      { name: "lection 26", url: "../" },
-      { name: "lection 27", url: "../" },
-    ],
-    weekTen: [
-      { name: "lection 28", url: "../" },
-      { name: "lection 29", url: "../" },
-      { name: "lection 30", url: "../" },
-    ],
-    weekEleven: [
-      { name: "lection 31", url: "../" },
-      { name: "lection 32", url: "../" },
-      { name: "lection 33", url: "../" },
-    ],
-    weekTwelve: [
-      { name: "lection 34", url: "../" },
-      { name: "lection 35", url: "../" },
-      { name: "lection 36", url: "../" },
-    ],
-    weekThirteen: [
-      { name: "lection 37", url: "../" },
-      { name: "lection 38", url: "../" },
-      { name: "lection 39", url: "../" },
-    ],
-    weekFourteen: [{ name: "lection 40", url: "../" }],
-  },
-];
+let jsonData = [];
 
+fetch(`https://api.jsonbin.io/v3/b/${binId}`, {
+  method: "GET",
+  headers: {
+    "X-Master-Key": masterKey,
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    jsonData = data.record.record;
+    displayData(jsonData);
+    console.log(jsonData);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+function displayData(data) {
+  const lectures = data[0];
+  const lectureInputsContainer = document.getElementById("lectureInputs");
+  for (const week in lectures) {
+    if (lectures.hasOwnProperty(week)) {
+      const lecturesInWeek = lectures[week];
+
+      lecturesInWeek.forEach((lecture) => {
+        const container = document.createElement("div");
+        container.classList.add("lecture__wrapper");
+
+        const input = document.createElement("input");
+        input.classList.add("lecture__input");
+        input.type = "text";
+        input.value = lecture.url;
+
+        const nameLabel = document.createElement("label");
+        nameLabel.classList.add("lecture__label");
+        nameLabel.textContent = lecture.name;
+
+        container.appendChild(nameLabel);
+        container.appendChild(input);
+
+        input.addEventListener("input", (event) => {
+          lecture.url = event.target.value;
+        });
+
+        lectureInputsContainer.appendChild(container);
+      });
+    }
+  }
+}
+
+const saveButton = document.getElementById("saveButton");
+saveButton.addEventListener("click", () => {
+  fetch("https://api.jsonbin.io/v3/b/65d477b61f5677401f31cdd7", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Master-Key":
+        "$2a$10$3AK4oCmUXowJtaZ1b60Wi.s0Pa4RJ/uZ2rHsCLbKohxTPqQmt7dLq",
+    },
+    body: JSON.stringify({ record: jsonData }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to save changes");
+      }
+      console.log("Changes saved successfully");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
