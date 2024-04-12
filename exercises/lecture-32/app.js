3c0927243b49ca22850d3b5e81a8687eaba854b7
Taras21071988 <tsasojlov@icloud.com>
2024-04-12 23:19:41 +0300

    I've added a little passphrase for those who like  to cheat.)))

--- exercises/lecture-32/app.js
+++ exercises/lecture-32/app.js
@@ -1,8 +1,8 @@
 const url = "https://jsonplaceholder.typicode.com/posts";
 
 const template = (item) => `
-  <h3>${item.title}</h3>
-  <div>${item.body}</div>
+  <h3>${item.title}...</h3>
+  <div>${item.body}...</div>
   <p>Author: <strong><span class="author" data-id="${item.userId}"></span></strong></p>
 `;
 
